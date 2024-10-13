49ce54ad208c052d3a7fe8087ff43ceacc9dfa88
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-13 20:22:30 +0600

    active links and conditional rendering

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,6 +22,7 @@ const Navbar = () => {
               className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
               aria-controls='mobile-menu'
               aria-expanded='false'
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
               <span className='absolute -inset-0.5'></span>
               <span className='sr-only'>Open main menu</span>
@@ -132,6 +133,7 @@ const Navbar = () => {
                     id='user-menu-button'
                     aria-expanded='false'
                     aria-haspopup='true'
+                    onClick={() => setIsProfileMenuOpen((prev) => !prev)}
                   >
                     <span className='absolute -inset-1.5'></span>
                     <span className='sr-only'>Open user menu</span>
@@ -144,19 +146,22 @@ const Navbar = () => {
                 </div>
 
                 {/* <!-- Profile dropdown --> */}
+
                 <div
                   id='user-menu'
-                  className='hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                  className={`${
+                    isProfileMenuOpen ? 'block' : 'hidden'
+                  } absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
                   role='menu'
                   aria-orientation='vertical'
                   aria-labelledby='user-menu-button'
-                  tabIndex='-1'
+                  tabIndex={-1}
                 >
                   <Link
                     href='/profile.html'
                     className='block px-4 py-2 text-sm text-gray-700'
                     role='menuitem'
-                    tabIndex='-1'
+                    tabIndex={-1}
                     id='user-menu-item-0'
                   >
                     Your Profile
@@ -165,7 +170,7 @@ const Navbar = () => {
                     href='/saved-properties.html'
                     className='block px-4 py-2 text-sm text-gray-700'
                     role='menuitem'
-                    tabIndex='-1'
+                    tabIndex={-1}
                     id='user-menu-item-2'
                   >
                     Saved Properties
@@ -173,7 +178,7 @@ const Navbar = () => {
                   <button
                     className='block px-4 py-2 text-sm text-gray-700'
                     role='menuitem'
-                    tabIndex='-1'
+                    tabIndex={-1}
                     id='user-menu-item-2'
                   >
                     Sign Out
@@ -186,7 +191,10 @@ const Navbar = () => {
       </div>
 
       {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-      <div className='hidden' id='mobile-menu'>
+      <div
+        className={`${isMobileMenuOpen ? 'block' : 'hidden'}`}
+        id='mobile-menu'
+      >
         <div className='space-y-1 px-2 pb-3 pt-2'>
           <Link
             href='/'
