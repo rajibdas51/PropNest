0bf601fd56e41c79801a14148f04e01782982396
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-17 20:09:59 +0600

    add demo properties.json file

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,7 +1,39 @@
+import Link from 'next/link';
 import React from 'react';
 
-const InfoBox = () => {
-  return <div>InfoBox</div>;
+type btnInfoType = {
+  text: string;
+  link: string;
+  btnBgColor: string;
+  hoverColor: string;
+};
+const InfoBox = ({
+  children,
+  heading,
+  bgColor = 'bg-gray-100',
+  txtColor = 'text-gray-800',
+  btnInfo,
+}: {
+  children: React.ReactNode;
+  heading: string;
+  bgColor: string;
+  txtColor: string;
+  btnInfo: btnInfoType;
+}) => {
+  return (
+    <>
+      <div className={`${bgColor} flex flex-col gap-4  p-6 rounded-lg `}>
+        <h2 className='text-2xl text-black font-bold'>{heading}</h2>
+        <p className='text-md font-bold'>{children}</p>
+        <Link
+          href={btnInfo.link}
+          className={`${btnInfo.btnBgColor}  inline-block w-max rounded-lg  text-white p-4 hover:${btnInfo.hoverColor}`}
+        >
+          Browse Properties
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default InfoBox;
--- components/InfoBoxes.tsx
+++ components/InfoBoxes.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link';
 import React from 'react';
+import InfoBox from './InfoBox';
 
 const InfoBoxes = () => {
-  return <div>InfoBoxes</div>;
+  return (
+    <>
+      <section>
+        <div className='container-xl lg:container m-auto'>
+          <div className='grid grid-col1 md:grid-cols-2 gap-6 p-4 '>
+            <InfoBox
+              heading='For Renters'
+              bgColor='bg-gray-100'
+              txtColor='text-gray-800'
+              btnInfo={{
+                text: 'Browse Properties',
+                link: '/properties',
+                btnBgColor: 'bg-black',
+                hoverColor: 'bg-gray-800',
+              }}
+            >
+              Find your dream rental property. Bookmark properties and contact
+              owners.
+            </InfoBox>
+            <InfoBox
+              heading='For Property Owners'
+              bgColor='bg-indigo-100'
+              txtColor='text-gray-800'
+              btnInfo={{
+                text: 'Add Property',
+                link: '/properties/add',
+                btnBgColor: 'bg-indigo-900',
+                hoverColor: 'bg-indigo-700',
+              }}
+            >
+              {' '}
+              List your properties and reach potential tenants. Rent as an
+              airbnb or long term.
+            </InfoBox>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default InfoBoxes;
