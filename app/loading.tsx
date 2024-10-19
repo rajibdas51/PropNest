472bf23403e2eb901b887e3738337da57fafee65
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-19 20:58:58 +0600

    add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+const loading = () => {
+  return (
+    <ClipLoader color='#6366F1' cssOverride={override} size={150}></ClipLoader>
+  );
+};
+
+export default loading;
