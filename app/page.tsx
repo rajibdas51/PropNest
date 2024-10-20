a16404a598dfd5887f92c6c62a7705b7e8adf33a
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-21 02:15:12 +0600

    add database config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import Image from 'next/image';
-
+import '@/app/globals.css';
+import connectDB from '@/config/database';
+connectDB();
 export default function Home() {
   return (
     <>
