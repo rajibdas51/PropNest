89166343cf280c1c33680337714fd2e37bf6edb5
Rajib Chandra Das <prorajib6@gmail.com>
2024-10-25 23:58:05 +0600

    save user to database & session

--- models/User.ts
+++ models/User.ts
@@ -3,22 +3,27 @@ import { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema(
   {
     email: {
-      type: {
-        type: String,
-        unique: [true, 'Email already exists.'],
-        required: [true, 'Email is required'],
-      },
-      username: {
-        type: String,
-        required: [true, 'User name is required! '],
-      },
-      image: {
-        type: String,
-      },
-      bookmarks: [{ type: Schema.Types.ObjectId, ref: 'Property' }],
+      type: String,
+      unique: [true, 'Email already exists'],
+      required: [true, 'Email is required'],
     },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property',
+      },
+    ],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const User = models.User || model('User', UserSchema);
--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,11 +1,13 @@
-import GoogleProvider from 'next-auth/providers/google';
-import { NextAuthOptions } from 'next-auth';
+import connectDB from '@/config/database';
+import User from '@/models/User';
 
-export const authOptions: NextAuthOptions = {
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           prompt: 'consent',
@@ -16,17 +18,34 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    // Invoked on successfull sign in
-    async signIn({ account, profile }) {
-      //1. Connect to the database
-      //2. Check if user exists
-      //3.if not, create user
-      //4. Return true to allow sign in
+    // Invoked on successful signin
+    async signIn({ profile }) {
+      // 1. Connect to database
+      await connectDB();
+      // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+      // 3. If not, then add user to database
+      if (!userExists) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+      // 4. Return true to allow sign in
+      return true;
     },
+    // Modifies the session object
     async session({ session }) {
-      //1.get user from database
-      //2.Assign user id from the session
-      //3.Rturn session
+      // 1. Get user from database
+      const user = await User.findOne({ email: session.user.email });
+      // 2. Assign the user id to the session
+      session.user.id = user._id.toString();
+      // 3. return session
+      return session;
     },
   },
 };
