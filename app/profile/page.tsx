7b46f527b8b01473cd25bd20aaa375fa6b2fe428
Rajib Chandra Das <prorajib6@gmail.com>
2024-11-03 14:00:28 +0600

    get userInfo from session

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,6 +5,11 @@ import profileDefault from '@/assets/images/profile.png';
 import connectDB from '@/config/database';
 const ProfilePage = async () => {
   await connectDB();
+  const sessionUser = await getSessionUser();
+  const { userId }: any = sessionUser;
+  if (!userId) {
+    throw new Error('User ID is required!');
+  }
   return (
     <section className='bg-indigo-50'>
       <div className='container m-auto py-24'>
@@ -13,18 +18,22 @@ const ProfilePage = async () => {
           <div className='flex flex-col md:flex-row'>
             <div className='md:w-1/4 mx-20 mt-10'>
               <div className='mb-4'>
-                <img
+                <Image
                   className='h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0'
-                  src='/images/profile.png'
+                  src={sessionUser?.user.image || profileDefault}
                   alt='User'
+                  width={200}
+                  height={200}
                 />
               </div>
 
               <h2 className='text-2xl mb-4'>
-                <span className='font-bold block'>Name: </span> John Doe
+                <span className='font-bold block'>Name: </span>{' '}
+                {sessionUser?.user.name}
               </h2>
               <h2 className='text-2xl'>
-                <span className='font-bold block'>Email: </span> john@gmail.com
+                <span className='font-bold block'>Email: </span>{' '}
+                {sessionUser?.user.email}
               </h2>
             </div>
 
