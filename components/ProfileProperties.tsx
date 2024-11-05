2db0a9a2ac41ed24fa11666d0dd28e8aaf095fb5
Rajib Chandra Das <prorajib6@gmail.com>
2024-11-05 21:47:08 +0600

    add ProfileProperties component

---
+++ components/ProfileProperties.tsx
@@ -0,0 +1,94 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+
+// Define types for nested objects within Property
+type Location = {
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+};
+
+type Rates = {
+  nightly: number | null;
+  weekly: number;
+  monthly: number;
+};
+
+type SellerInfo = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+// Define the main Property type
+type Property = {
+  _id: string; // Typically, ObjectId is represented as a string in TypeScript
+  owner: string;
+  name: string;
+  type: string;
+  description: string;
+  location: Location;
+  beds: number;
+  baths: string;
+  square_feet: number;
+  amenities: string[];
+  rates: Rates;
+  seller_info: SellerInfo;
+  images: string[];
+  is_featured: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type PropertiesType = {
+  Property: Property[];
+};
+const ProfileProperties = ({
+  properties: initialProperties,
+}: {
+  properties: PropertiesType;
+}) => {
+  const [properties, setProperties] = useState(initialProperties);
+  return (
+    <>
+      {properties.map((property: Property) => (
+        <div className='mb-10' key={property._id}>
+          <a href='/property.html'>
+            <img
+              className='h-32 w-full rounded-md object-cover'
+              src={`${property.images[0]}`}
+              alt='Property 1'
+              width={1000}
+              height={2}
+            />
+          </a>
+          <div className='mt-2'>
+            <p className='text-lg font-semibold'>{property.name}</p>
+            <p className='text-gray-600'>
+              Address: {property.location.street} {property.location.city}{' '}
+              {property.location.state} {property.location.zipcode}
+            </p>
+          </div>
+          <div className='mt-2'>
+            <a
+              href='/add-property.html'
+              className='bg-indigo-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-indigo-600'
+            >
+              Edit
+            </a>
+            <button
+              className='bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600'
+              type='button'
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ProfileProperties;
